eb859e9be65dfeb229d06c4ab0afa2f66feb983b
Gideon Nosike <csgideons04@gmail.com>
2022-09-22 15:22:49 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a' ; a <= 'z' ; a++)
+		putchar (a);
+		putchar ('\n');
+	
+	return (0);
+}
