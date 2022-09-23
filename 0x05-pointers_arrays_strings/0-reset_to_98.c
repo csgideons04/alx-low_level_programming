f247e362854fe74e3276ae998e02314e26c8b3cd
Gideon Nosike <csgideons04@gmail.com>
2022-09-23 03:12:20 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+*
+**_strcat - function
+*
+*@dest:char parameter
+*
+*@src: char parameter
+*
+*Return: char value des
+*
+*/
+
+
+char *_strcat(char *dest, char *src)
+{
+	int len1, len2, i;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+
+	for(i = 0; i<=len2; i++)
+	{
+		dest[len1+i] = src[i];
+	}
+	return (dest);
+}
+
+// int main()
+//
+// {
+//
+//      char s1[] = "Hello ";
+//
+//      char s2[] = "Adriel\n";
+//
+//     char *ptr;
+//
+//
+//
+//      ptr = _strcat(s1, s2);
+//
+//
+//
+//      printf("%s", ptr);
+//
+//
+//
+//      return (0);
+//
+// }
